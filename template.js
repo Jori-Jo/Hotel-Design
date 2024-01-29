76f5c493547ebbd3d80aac4aa69010d1b6c1ba4c
Jori-Jo <jorisha.joshi@codemanas.com>
2024-01-29 13:34:02 +0545

    popup box disappear when clicked on background

--- template.js
+++ template.js
@@ -12,6 +12,34 @@ const closeModalWindow = () => {
 }
 closeModalButton.addEventListener("click", closeModalWindow);
 
+const popupOverlay = (event) =>{
+    if (event.target === modalWindowOverlay) {
+        modalWindowOverlay.style.display = 'none';
+    }
+}
+modalWindowOverlay.addEventListener("click", popupOverlay);
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const showPopupBtn = document.getElementById('open-modal');
+//     const popupOverlay = document.querySelector(".popup");
+//     const closePopupBtn = document.getElementById('close-modal');
+
+//     showPopupBtn.addEventListener('click', function () {
+//         popupOverlay.style.display = 'flex';
+//     });
+
+//     closePopupBtn.addEventListener('click', function () {
+//         popupOverlay.style.display = 'none';
+//     });
+
+//     popupOverlay.addEventListener('click', function (event) {
+//         // Check if the clicked element is the overlay (background) and hide the popup
+//         if (event.target === popupOverlay) {
+//             popupOverlay.style.display = 'none';
+//         }
+//     });
+// });
 
 
 
