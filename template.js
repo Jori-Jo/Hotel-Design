ed40ce81533ef53b79cc1f46805679a1f163623f
Jori-Jo <jorisha.joshi@codemanas.com>
2024-01-29 14:55:28 +0545

    popup box disappear when clicked outside issue fixed

--- template.js
+++ template.js
@@ -1,24 +1,23 @@
-const openModalButton = document.getElementById("open-modal");
 const modalWindowOverlay = document.querySelector(".popup");
+const openModalButton = document.getElementById("open-modal");
+const closeModalButton = document.getElementById("close-modal");
+
 const showModalWindow = () => {
     modalWindowOverlay.classList.add('active');
 }
-openModalButton.addEventListener("click", showModalWindow);
 
-
-const closeModalButton = document.getElementById("close-modal");
 const closeModalWindow = () => {
     modalWindowOverlay.classList.remove('active');
 }
-closeModalButton.addEventListener("click", closeModalWindow);
 
 const popupOverlay = (event) =>{
     if (event.target === modalWindowOverlay) {
-        modalWindowOverlay.style.display = 'none';
+        modalWindowOverlay.classList.remove('active');
     }
 }
+openModalButton.addEventListener("click", showModalWindow);
+closeModalButton.addEventListener("click", closeModalWindow);
 modalWindowOverlay.addEventListener("click", popupOverlay);
-
 
 // document.addEventListener('DOMContentLoaded', function () {
 //     const showPopupBtn = document.getElementById('open-modal');
@@ -39,6 +38,7 @@ modalWindowOverlay.addEventListener("click", popupOverlay);
 //             popupOverlay.style.display = 'none';
 //         }
 //     });
+    
 // });
 
 
