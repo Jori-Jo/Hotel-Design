eac08edf4e058268e18a3e4c96f6bf21e1d383a8
Jori-Jo <jorisha.joshi@codemanas.com>
2024-01-30 17:48:35 +0545

    popup in vanilla js for two buttons by class name

--- template.js
+++ template.js
@@ -1,3 +1,53 @@
+
+// /*Popup in Vanilla JS for buttons using ID*/
+// const modalWindowOverlay1 = document.getElementById("popup1");
+// const modalWindowOverlay2 = document.getElementById("popup2");
+
+// const openModalButton1 = document.getElementById("open-modal1");
+// const closeModalButton1 = document.getElementById("close-modal1");
+
+// const openModalButton2 = document.getElementById("open-modal2");
+// const closeModalButton2 = document.getElementById("close-modal2");
+
+// const showModalWindow1 = () => {
+//     modalWindowOverlay1.style.display='block';
+// }
+
+// const closeModalWindow1 = () => {
+//     modalWindowOverlay1.style.display='none';
+// }
+
+// const showModalWindow2 = () => {
+//     modalWindowOverlay2.style.display='block';
+// }
+
+// const closeModalWindow2 = () => {
+//     modalWindowOverlay2.style.display='none';
+// }
+
+// const popupOverlay1 = (event) =>{
+//     if (event.target === modalWindowOverlay1) {
+//         modalWindowOverlay1.style.display='none';
+//     }
+// }
+
+// const popupOverlay2 = (event) =>{
+//     if (event.target === modalWindowOverlay2) {
+//         modalWindowOverlay2.style.display='none';
+//     }
+// }
+
+// openModalButton1.addEventListener("click", showModalWindow1);
+// closeModalButton1.addEventListener("click", closeModalWindow1);
+// modalWindowOverlay1.addEventListener("click", popupOverlay1);
+
+// openModalButton2.addEventListener("click", showModalWindow2);
+// closeModalButton2.addEventListener("click", closeModalWindow2);
+// modalWindowOverlay2.addEventListener("click", popupOverlay2);
+
+
+
+/*Popup in Vanilla JS for one button*/
 // const modalWindowOverlay = document.querySelector(".popup");
 // const openModalButton = document.getElementById("open-modal");
 // const closeModalButton = document.getElementById("close-modal");
@@ -19,71 +69,7 @@
 // closeModalButton.addEventListener("click", closeModalWindow);
 // modalWindowOverlay.addEventListener("click", popupOverlay);
 
-
-const modalWindowOverlay1 = document.getElementById("popup1");
-const modalWindowOverlay2 = document.getElementById("popup2");
-
-const openModalButton1 = document.getElementById("open-modal1");
-const closeModalButton1 = document.getElementById("close-modal1");
-
-const openModalButton2 = document.getElementById("open-modal2");
-const closeModalButton2 = document.getElementById("close-modal2");
-
-const showModalWindow1 = () => {
-    modalWindowOverlay1.style.display='block';
-}
-
-const closeModalWindow1 = () => {
-    modalWindowOverlay1.style.display='none';
-}
-
-const showModalWindow2 = () => {
-    modalWindowOverlay2.style.display='block';
-}
-
-const closeModalWindow2 = () => {
-    modalWindowOverlay2.style.display='none';
-}
-
-const popupOverlay1 = (event) =>{
-    if (event.target === modalWindowOverlay1) {
-        modalWindowOverlay1.style.display='none';
-    }
-}
-
-const popupOverlay2 = (event) =>{
-    if (event.target === modalWindowOverlay2) {
-        modalWindowOverlay2.style.display='none';
-    }
-}
-
-openModalButton1.addEventListener("click", showModalWindow1);
-closeModalButton1.addEventListener("click", closeModalWindow1);
-modalWindowOverlay1.addEventListener("click", popupOverlay1);
-
-openModalButton2.addEventListener("click", showModalWindow2);
-closeModalButton2.addEventListener("click", closeModalWindow2);
-modalWindowOverlay2.addEventListener("click", popupOverlay2);
-
-
-
-// const openModalButtons = document.querySelectorAll(".open-modal");
-// const closeModalButtons = document.querySelectorAll(".close-modal");
-// const modalWindowOverlays = document.querySelectorAll(".popup");
-
-
-// openModalButtons.forEach(button => {
-//     button.addEventListener("click", function() {
-//         const popupId = this.getAttribute("data-popup");
-//         const popup = document.getElementById(popupId);
-//         popup.classList.add('active');
-//     });
-// });
-
-// closeModalButtons.forEach(button => button.addEventListener("click", function() {
-//     this.closest('.popup').classList.remove('active');
-// }));
-
+/*2*/
 // document.addEventListener('DOMContentLoaded', function () {
 //     const showPopupBtn = document.getElementById('open-modal');
 //     const popupOverlay = document.querySelector(".popup");
@@ -98,14 +84,40 @@ modalWindowOverlay2.addEventListener("click", popupOverlay2);
 //     });
 
 //     popupOverlay.addEventListener('click', function (event) {
-//         // Check if the clicked element is the overlay (background) and hide the popup
 //         if (event.target === popupOverlay) {
 //             popupOverlay.style.display = 'none';
 //         }
 //     });
-    
+
 // });
 
 
+const button = document.querySelectorAll('.button')
+const popup = document.querySelector('.popup')
+const closeWindow = document.querySelector('.close-modal')
+
+function addActive(element) {
+    element.classList.add('active');
+}
+
+function removeActive(element){
+    element.classList.remove('active');
+}
+
+button.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        addActive(popup);
+    })
+})
+
+closeWindow.addEventListener('click', () => {
+    removeActive(popup);
+})
+
+popup.addEventListener('click', function (event) {
+    if (event.target === popup) {
+        popup.classList.remove('active');
+    }
+});
 
 
