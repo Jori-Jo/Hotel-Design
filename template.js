eed88a404e7ad877fe5c02e4717f2f083cffd1aa
Jori-Jo <jorisha.joshi@codemanas.com>
2024-02-02 17:59:58 +0545

    hamburger menu for menubar for popup only completed

--- template.js
+++ template.js
@@ -1,57 +1,3 @@
-
-
-/*Popup in Vanilla JS for one button*/
-// const modalWindowOverlay = document.querySelector(".popup");
-// const openModalButton = document.getElementById("open-modal");
-// const closeModalButton = document.getElementById("close-modal");
-
-// const showModalWindow = () => {
-//     modalWindowOverlay.classList.add('active');
-// }
-
-// const closeModalWindow = () => {
-//     modalWindowOverlay.classList.remove('active');
-// }    
-
-// const popupOverlay = (event) =>{
-//     if (event.target === modalWindowOverlay) {
-//         modalWindowOverlay.classList.remove('active');
-//     }
-// }
-// openModalButton.addEventListener("click", showModalWindow);
-// closeModalButton.addEventListener("click", closeModalWindow);
-// modalWindowOverlay.addEventListener("click", popupOverlay);
-
-
-// Popup in Vanilla  JS for two buttons
-// const button = document.querySelectorAll('.button')
-// const popup = document.querySelector('.popup')
-// const closeWindow = document.querySelector('.close-modal')
-
-// function addActive(element) {
-//     element.classList.add('active');
-// }
-
-// function removeActive(element){
-//     element.classList.remove('active');
-// }
-
-// button.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         addActive(popup);
-//     })
-// })
-
-// closeWindow.addEventListener('click', () => {
-//     removeActive(popup);
-// })
-
-// popup.addEventListener('click', function (event) {
-//     if (event.target === popup) {
-//         removeActive(popup);
-//     }
-// });
-
 /*Popup in Vanila JS in IIFE*/
 
 (function () {
@@ -108,5 +54,18 @@
     document.addEventListener('DOMContentLoaded', popup().init)
 })()
 
+/*HAmburger menu*/
 
+const openButton = document.querySelector('.menu-bar');
+const showMenu = document.querySelector('.hamburger');
+// const closeButton = document.querySelector('.close-button')
+
+const openMenu = () =>{
+    showMenu.classList.add('active');
+}
+
+openButton.addEventListener("click", openMenu);
+// const closeMenu = () =>{
+//     showMenu.classList.remove('active');
+// }
 
